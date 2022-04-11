ede98f8e2b0fb0b45593fd4d81db6305751d3a45
Bartłomiej Chmiel <bachm44@gmail.com>
2022-04-11 10:28:23 +0200

    Init backend

---
+++ backend/src/test/java/com/tlx/tlx/TlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlx.tlx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
