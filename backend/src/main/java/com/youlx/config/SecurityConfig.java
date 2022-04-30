17196c5c76c73c58795c2d52650cc80282810933
Bartłomiej Chmiel <bachm44@gmail.com>
2022-04-30 18:57:43 +0200

    Custom logout url

--- backend/src/main/java/com/youlx/config/SecurityConfig.java
+++ backend/src/main/java/com/youlx/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .exceptionHandling()
                 .defaultAuthenticationEntryPointFor(
                         new Http403ForbiddenEntryPoint(),
@@ -45,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/api/login")
                 .and()
                 .logout()
+                .logoutUrl("/api/logout")
                 .permitAll();
     }
 
