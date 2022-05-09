7fcd9f1f54a7e908b447073357da335258af81c8
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-05-09 17:31:11 +0200

    frontend: fixed tests

--- frontend/src/app/pages/offers/components/offer-new/offer-new.component.spec.ts
+++ frontend/src/app/pages/offers/components/offer-new/offer-new.component.spec.ts
@@ -1,16 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { OfferNewComponent } from './offer-new.component';
+import {OfferNewComponent} from './offer-new.component';
+import {AuthService} from '../../../../services/auth.service';
+import {OffersService} from '../../../../services/offers.service';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('OfferNewComponent', () => {
   let component: OfferNewComponent;
   let fixture: ComponentFixture<OfferNewComponent>;
+  let offersServiceSpy: any = jasmine.createSpyObj('OffersService', ['createOffer']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OfferNewComponent ]
+      declarations: [OfferNewComponent],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ],
+      providers: [
+        {provide: OffersService, useValue: offersServiceSpy},
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
